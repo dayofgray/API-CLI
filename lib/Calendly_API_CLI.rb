881b0d48fa0f38d6a46b0f5ca32fd51bf58b8513
Grayson <graysonpberger@gmail.com>
2020-04-09 20:54:52 -0400

    first commit

---
+++ lib/Calendly_API_CLI.rb
@@ -0,0 +1,6 @@
+require "Calendly_API_CLI/version"
+
+module CalendlyAPICLI
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/Calendly_API_CLI_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe CalendlyAPICLI do
+  it "has a version number" do
+    expect(CalendlyAPICLI::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
